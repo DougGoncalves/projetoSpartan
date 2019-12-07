a21e36c13e31536fa6d45c9d1a6d0a022b572fed
DougGoncalves <doug.goncalvesf@gmail.com>
2019-12-07 10:38:14 -0300

    Atualização servico.php

--- servico.php
+++ servico.php
@@ -6,14 +6,22 @@
             <h1><?php echo getNome($_GET['id']); ?></h1>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-6 mt-3 mb-3">
-            <p><?php echo getDescricao($_GET['id']); ?></p>
-        </div>
-        <div class="col-lg-6 text-center">
-            <img src="<?php echo getImagem($_GET['id']); ?>" class="ilustra-servico" alt="<?php echo getNome($_GET['id']); ?>">
-        </div>
-    </div>
+    <?php 
+        $servicos = listarServicos ();
+
+        foreach ($servicos as $indice => $valor){
+            echo "<div class='row'>
+            <div class='col-lg-6 mt-3 mb-3'>
+                <p><a href='servico.php?id=$indice'>".$servicos[$indice]['descricao']."</a></p>
+            </div>
+            <div class='col-lg-6 text-center'>
+                <img src=".
+                $servicos[$indice]['imagem']." class='ilustra-servico' alt=".
+                $servicos[$indice]['nome'].">
+            </div>
+            </div>";
+        }
+    ?> 
 </section>
 
  <?php require_once('footer.php'); ?>
